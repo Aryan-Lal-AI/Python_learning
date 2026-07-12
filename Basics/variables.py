4d1a801d13b0754d6feb9d3bde989da735f82a2e
Aryan Lal <aryan22gg@gmail.com>
2026-07-12 18:18:04 +0300

    Added variables and print statements for my description
    This file contains variable declarations and a basic if/else statement. It includes strings, integers, floats, and booleans, along with print statements to describe me.

---
+++ Basics/variables.py
@@ -0,0 +1,39 @@
+# Greetings!
+# In this file I will be practicing with variables
+# These include strings, integers, floats and booleans
+# I will also be working with a basic if/else statement
+
+# Strings
+first_name = "Aryan"
+fav_hobby = "reading"
+fav_book = "The Picture Of Dorian Gray"
+
+# Integers
+age = 17
+fav_number = 5
+year = 2026
+
+# Floats
+distance = 7.2
+book_price = 11.99
+average_grade = 18.7
+
+# Booleans
+is_available = True
+
+
+# Now I will create a small description of myself using print statements
+# I will use the variables alongside with f strings
+
+print(f"Hello! My name is {first_name}")
+print(f"I am {age} years old")
+print(f"I got an average of {average_grade}/20 in {year} at high school")
+print(f"My school is {distance}km away so I take the bus")
+print(f"My favorite number is {fav_number}")
+print(f"My favorite hobby is {fav_hobby}")
+print(f"My favorite book is {fav_book} and it cost me {book_price}€")
+if is_available:
+    print("I am available")
+else:
+    print("I am unavailable")
+print("Thanks for viewing! I will continue learning!")
