85712f0c6f8bba4ca9d5e8b3d96ba80ccc8c2321
Aryan Lal <aryan22gg@gmail.com>
2026-07-17 19:40:25 +0300

    Created a basic madlibs game script
    This script implements a basic madlibs game that prompts the user for various inputs to create a fun story.

---
+++ Basics/mini_projects/madlibs.py
@@ -0,0 +1,37 @@
+# Greetings!
+# I will be creating a basic madlibs game to practice user input
+# The story I'll be using can be found online
+
+name_1 = input("Please enter your name: ")
+number_1 = input("Please enter a number: ")
+adjective_1 = input("Please enter an adjective: ")
+color = input("Please enter a color: ")
+noun_1 = input("Please enter a noun: ")
+food = input("Please enter a type of food in plural: ")
+noun_2 = input("Please enter a noun: ")
+verb = input("Please enter a verb ending with -ing: ")
+clothing = input("Please enter a piece of clothing: ")
+adjective_2 = input("Please enter an adjective: ")
+celebrity = input("Please enter the name of a celebrity: ")
+number_2 = input("Please enter a number: ")
+name_2 = input("Please enter a name of a person you know: ")
+noun_3 = input("Please enter a noun: ")
+name_3 = input("Please enter a name of a person you know: ")
+occupation = input("Please enter an occupation: ")
+
+print(f"My name is {name_1} and I am {number_1} years old.")
+print(f"If I were president, I'd do a whole bunch of {adjective_1} things:")
+print(f"1. I would drive the biggest {color} car in the country.")
+print(f"And that car would go faster than any other {noun_1} in the world!")
+print(f"2. Everyone would eat {food} for dinner!")
+print(
+    f"3. I would live in the statue of {noun_2} and build a {verb} at her feet.")
+print(
+    f"4. I would wear a/an {clothing} on my head, and everyone would say I look {adjective_2} like {celebrity}.")
+print(f"5. Schools would be open only {number_2} days a year.")
+print("6. I would give my friends the best jobs.")
+print(f"I would nominate {name_2} to be the secretery of (the) {noun_3}.")
+print(f"And {name_3} could be vice {occupation}!")
+
+# This was it!
+# Thank you for viewing!
